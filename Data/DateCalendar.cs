db9009e3ca30861b99c7d7f28ee055ddda3f8cc3
Vadim Salajan <vadim.salajan@outlook.com>
2023-03-28 16:02:43 +0300

    firstCommit

---
+++ Data/DateCalendar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossCalendar.Data
+{
+    public class DateCalendar
+    {
+        public DateTime Day { get; set; }
+    }
+}
